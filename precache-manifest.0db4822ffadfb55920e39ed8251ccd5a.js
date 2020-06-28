ade364bebf2cd553b1be8f6a0f539d11114f6259
albinjal <albin.jaldevik@gmail.com>
2020-06-29 00:52:47 +0200

    Updates

---
+++ precache-manifest.0db4822ffadfb55920e39ed8251ccd5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7866bb763638a04fe72ce14fc48392fe",
+    "url": "/unbeatable-tictactoe/index.html"
+  },
+  {
+    "revision": "0d1c0c63fdd32930bf83",
+    "url": "/unbeatable-tictactoe/static/css/main.8578a8f7.chunk.css"
+  },
+  {
+    "revision": "85193807b25e742113e8",
+    "url": "/unbeatable-tictactoe/static/js/2.90c69ce2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unbeatable-tictactoe/static/js/2.90c69ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d1c0c63fdd32930bf83",
+    "url": "/unbeatable-tictactoe/static/js/main.7561e388.chunk.js"
+  },
+  {
+    "revision": "4a9764921b9e6f8bc075",
+    "url": "/unbeatable-tictactoe/static/js/runtime-main.0398f7d8.js"
+  }
+]);
