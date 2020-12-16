f1d8768bd2cf93c42845ee4343149e974f2a5ae3
albinjal <albin.jaldevik@gmail.com>
2020-12-16 21:14:06 +0100

    changable delay

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { BoardState } from '../../logic/board';
 import React from 'react';
 import Board from './Board';
@@ -7,12 +7,19 @@ import { calculateBestMove } from '../../logic/bot';
 import { GameStats } from './GameStats';
 import { SnackbarContext } from '../snackbars/SnackbarWrapper';
 
+export enum GameModes {
+  PvP,
+  PvB,
+  BvB,
+}
+
 export const Player = Outcomes.Cross;
 
 const Game: React.FunctionComponent = () => {
   const [startingNext, setStarting] = useState<Players>(Player);
   const [boardState, setBoard] = useState<BoardState>(new BoardState(startingNext));
-  const [score, setScore] = useState<Record<Players, number>>({ [Outcomes.Circle]: 0, [Outcomes.Cross]: 0 });
+  const [mode, setMode] = useState(GameModes.PvB);
+  const [delay, setDelay] = useState(700);
   const { openSnackbar } = useContext(SnackbarContext);
 
   useEffect(() => {
@@ -32,7 +39,7 @@ const Game: React.FunctionComponent = () => {
     // Delay for dramatic effect
     setTimeout(() => {
       makeMove(optimalMove);
-    }, 500);
+    }, delay * 50);
   };
 
   const makeMove = (tile: number) => {
@@ -50,19 +57,28 @@ const Game: React.FunctionComponent = () => {
     }
   };
   const gameOver = () => {
-    if (boardState.winner) {
-      const newScore = score;
-      ++newScore[otherPlayer(Player)];
-      setScore(newScore);
-    }
     openSnackbar(`Game Over. ${boardState.winner ? 'Bot Won' : 'Draw'}!`);
     setTimeout(restart, 1000);
+  };
+
+  const changeBotDelay = (ms: number) => {
+    setDelay(ms);
+  };
+
+  const changeMode = (mode: GameModes) => {
+    setMode(mode);
   };
 
   return (
     <div className="flex w-full flex-wrap justify-center items-center flex-grow">
       <Board board={boardState} makeMove={playerMakeMove} />
-      <GameStats boardState={boardState} score={score} />
+      <GameStats
+        boardState={boardState}
+        changeMode={changeMode}
+        changeDelay={changeBotDelay}
+        botDelay={delay}
+        mode={mode}
+      />
     </div>
   );
 };
--- src/components/game/GameStats.tsx
+++ src/components/game/GameStats.tsx
@@ -1,25 +1,35 @@
+import { Slider } from '@material-ui/core';
 import React from 'react';
 import { BoardState } from '../../logic/board';
-import { otherPlayer, Players } from '../../logic/outcomes';
-import { Player } from './Game';
+import { otherPlayer } from '../../logic/outcomes';
+import { GameModes, Player } from './Game';
 
+const delayScale = 50;
 interface Props {
   boardState: BoardState;
-  score: Record<Players, number>;
+  changeDelay: (delay: number) => void;
+  changeMode: (mode: GameModes) => void;
+  botDelay: number;
+  mode: GameModes;
 }
 
-export const GameStats: React.FunctionComponent<Props> = ({ boardState, score }: Props) => {
+export const GameStats: React.FunctionComponent<Props> = ({
+  boardState,
+  botDelay,
+  mode,
+  changeDelay,
+  changeMode,
+}: Props) => {
+  console.log(botDelay);
   return (
     <div className="flex flex-col p-4 items-center bg-white shadow rounded m-2 w-full max-w-md border">
       <h1 className="text-3xl text-center font-light">Unbeatable TicTacToe</h1>
-      <h2>
-        Bot {score[otherPlayer(Player)]} – {score[Player]} You
-      </h2>
       <hr className="border-t w-4/5 my-1 border-celadon-blue" />
       <h3>{boardState.playerTurn === Player ? 'Your' : 'Bot'} turn</h3>
 
       <h2>Settings</h2>
       <hr className="border-t w-4/5 my-1 border-celadon-blue" />
+      <Slider value={botDelay} onChange={(event, newValue) => changeDelay(newValue as number)}></Slider>
     </div>
   );
 };
